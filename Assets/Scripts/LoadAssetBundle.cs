57cfbf6fdc00ff528b60c034574a2362a1ef9e1c
EtDuxDeus <minerukraine@gmail.com>
2021-05-24 09:19:55 +0300

    minor commit, Added namespace to LoadAssetScript

--- Assets/Scripts/LoadAssetBundle.cs
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LoadAssetBundle : MonoBehaviour
+
+namespace Tools
 {
-    private Image _imageHolder;
-
-
-    private void Awake()
+    public class LoadAssetBundle : MonoBehaviour
     {
-        _imageHolder = GetComponent<Image>();
-    }
+        private Image _imageHolder;
 
 
-    private void Start()
-    {
-        LoadTextureFromFile();
-    }
+        private void Awake()
+        {
+            _imageHolder = GetComponent<Image>();
+        }
 
 
-    private void LoadTextureFromFile()
-    {
-        AssetBundle loadedAssetBundle = AssetBundle.LoadFromFile(Application.dataPath + "/AssetBundles/nekospritebundle.sprites");
-        Texture2D spriteForImageHolder = loadedAssetBundle.LoadAsset<Texture2D>("53f98a6b76f60356b2b4c261963377e6");
-        _imageHolder.sprite = Sprite.Create(spriteForImageHolder, new Rect(0.0f, 0.0f, spriteForImageHolder.width, spriteForImageHolder.height), new Vector2(0.5f, 0.5f), 100.0f);
+        private void Start()
+        {
+            LoadTextureFromFile();
+        }
+
+
+        private void LoadTextureFromFile()
+        {
+            AssetBundle loadedAssetBundle = AssetBundle.LoadFromFile(Application.dataPath + "/AssetBundles/nekospritebundle.sprites");
+            Texture2D spriteForImageHolder = loadedAssetBundle.LoadAsset<Texture2D>("53f98a6b76f60356b2b4c261963377e6");
+            _imageHolder.sprite = Sprite.Create(spriteForImageHolder, new Rect(0.0f, 0.0f, spriteForImageHolder.width, spriteForImageHolder.height), new Vector2(0.5f, 0.5f), 100.0f);
+        }
     }
 }
